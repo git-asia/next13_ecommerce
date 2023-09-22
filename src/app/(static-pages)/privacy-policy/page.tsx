c79bf03fa042315117bf3bc2ebfb431d1ace4252
Joanna <tuszynska.joanna@gmail.com>
2023-09-22 22:54:05 +0200

    Created group of pages with terms and policy

---
+++ src/app/(static-pages)/privacy-policy/page.tsx
@@ -0,0 +1,47 @@
+export default function PrivacyPolicy() {
+	return (
+		<div className="text-black">
+			<h1 className="mb-4 text-2xl font-semibold">Privacy Policy</h1>
+			<div className="mb-2">
+				<p>
+					This Privacy Policy explains how [Your Company Name]
+					collects, uses, and protects your personal information when
+					you use our e-commerce shop.
+				</p>
+				<p>
+					<strong>Information We Collect:</strong>
+					We may collect personal information such as your name, email
+					address, shipping address, and payment information when you
+					make a purchase on our website.
+				</p>
+				<p>
+					<strong>How We Use Your Information:</strong>
+					We use your information to process and fulfill your orders,
+					communicate with you about your orders, and improve our
+					services.
+				</p>
+				<p>
+					<strong>Security:</strong>
+					We take the security of your personal information seriously
+					and implement measures to protect it.
+				</p>
+				<p>
+					<strong>Third-Party Services:</strong>
+					We may use third-party services that collect and process
+					your information, such as payment processors. Please review
+					their privacy policies.
+				</p>
+				<p>
+					<strong>Updates to Privacy Policy:</strong>
+					We may update this Privacy Policy to reflect changes in our
+					practices. Check this page for the latest information.
+				</p>
+				<p>
+					If you have any questions about our Privacy Policy, please
+					contact us at [Your Contact Information].
+				</p>
+				<p>This Privacy Policy was last updated on [Date].</p>
+			</div>
+		</div>
+	);
+}
---
+++ src/app/(static-pages)/terms-and-conditions/page.tsx
@@ -0,0 +1,37 @@
+export default function TermsAndConditions() {
+    return (
+      <div className="text-black">
+        <h1 className="mb-4 text-2xl font-semibold">Terms and Conditions</h1>
+        <div className="mb-2">
+          <p>
+            These Terms and Conditions govern your use of [Your Company Name] e-commerce shop. By accessing or using our website, you agree to comply with these terms.
+          </p>
+          <p>
+            <strong>Use of Our Website:</strong>
+            You must use our website in accordance with applicable laws and regulations. Any unauthorized use or access is prohibited.
+          </p>
+          <p>
+            <strong>Intellectual Property:</strong>
+            All content on our website, including text, images, and logos, is our intellectual property and may not be used without permission.
+          </p>
+          <p>
+            <strong>Privacy:</strong>
+            Your use of our website is also governed by our Privacy Policy. Please review it for information on how we collect and use your data.
+          </p>
+          <p>
+            <strong>Disclaimer:</strong>
+            We provide our website on an &quot;as is&quot; basis. We do not guarantee the accuracy or availability of our website.
+          </p>
+          <p>
+            <strong>Changes to Terms:</strong>
+            We may update these Terms and Conditions at any time. It&rsquo;s your responsibility to review them periodically.
+          </p>
+          <p>
+            If you have any questions about our Terms and Conditions, please contact us at [Your Contact Information].
+          </p>
+          <p>This Terms and Conditions was last updated on [Date].</p>
+        </div>
+      </div>
+    );
+  }
+  
