d56ebfe601f8633c91acacd1899abea74a3c86f0
Joanna <tuszynska.joanna@gmail.com>
2023-09-22 22:47:25 +0200

    Moved products data to separate file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,6 @@
+import { productsData } from "./productsData";
 import { ProductsList } from "@/ui/organisms/ProductsList";
-import { type ProductItemType } from "@/ui/types";
-
-const products: ProductItemType[] = [
-	{
-		id: "1",
-		category: "Accessories",
-		name: "Sling",
-		price: 2500,
-		coverImage: {
-			alt: "",
-			src: "/product_1.png",
-		},
-	},
-	{
-		id: "2",
-		category: "Cloths",
-		name: "T-shirt",
-		price: 25000,
-		coverImage: {
-			alt: "",
-			src: "/product_2.png",
-		},
-	},
-	{
-		id: "3",
-		category: "Accessories",
-		name: "Harness",
-		price: 12500,
-		coverImage: {
-			alt: "",
-			src: "/product_3.png",
-		},
-	},
-	{
-		id: "4",
-		category: "Accessories",
-		name: "Rope",
-		price: 45000,
-		coverImage: {
-			alt: "",
-			src: "/product_4.png",
-		},
-	},
-];
 
 export default function Home() {
-	return (
-		<section className="sm-py-10 mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
-			<ProductsList products={products} />
-		</section>
-	);
+	return <ProductsList products={productsData} />;
 }
---
+++ src/app/productsData.ts
@@ -0,0 +1,44 @@
+import { type ProductItemType } from "@/ui/types";
+
+export const productsData: ProductItemType[] = [
+	{
+		id: "1",
+		category: "Ice&snow",
+		name: "Crampons",
+		price: 2500,
+		coverImage: {
+			alt: "",
+			src: "/product_1.png",
+		},
+	},
+	{
+		id: "2",
+		category: "Cloths",
+		name: "T-shirt",
+		price: 25000,
+		coverImage: {
+			alt: "",
+			src: "/product_2.png",
+		},
+	},
+	{
+		id: "3",
+		category: "Accessories",
+		name: "Harness",
+		price: 12500,
+		coverImage: {
+			alt: "",
+			src: "/product_3.png",
+		},
+	},
+	{
+		id: "4",
+		category: "Accessories",
+		name: "Rope",
+		price: 45000,
+		coverImage: {
+			alt: "",
+			src: "/product_4.png",
+		},
+	},
+];
